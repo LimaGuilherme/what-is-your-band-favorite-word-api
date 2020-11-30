ccb0546c4c160136582766a30faf33a47f5dc8f0
guilherme <guilherme@inceres.com.br>
2020-11-30 11:48:56 -0300

    Fixing test.

--- src/tests/unit/domain/test_application_service.py
+++ src/tests/unit/domain/test_application_service.py
@@ -8,6 +8,7 @@ class TestArtistLyricsService(TestCase):
 
     def test_count_frequency_works(self):
         lyrics_searcher_mock = MagicMock()
+        artist_searcher_mock = MagicMock()
 
         statistic_mock = MagicMock()
         list_statistic_mock = MagicMock()
@@ -17,7 +18,10 @@ class TestArtistLyricsService(TestCase):
         repository_mock = MagicMock()
         repository_mock.get_by_artist.return_value = lyrics_list_mock
 
-        artist_lyrics_service = ArtistLyricsService(lyrics_searcher_mock, statistic_mock, repository_mock)
+        artist_lyrics_service = ArtistLyricsService(lyrics_searcher_mock,
+                                                    statistic_mock,
+                                                    repository_mock,
+                                                    artist_searcher_mock)
         result = artist_lyrics_service.count_frequency('Weiss')
 
         repository_mock.get_by_artist.assert_called_with('Weiss')
@@ -25,6 +29,7 @@ class TestArtistLyricsService(TestCase):
         self.assertEqual(list_statistic_mock, result)
 
     def test_index_works(self):
+        artist_searcher_mock = MagicMock()
         lyrics_mock1 = MagicMock()
         lyrics_mock2 = MagicMock()
         lyrics_searcher_mock = MagicMock()
@@ -34,7 +39,10 @@ class TestArtistLyricsService(TestCase):
 
         repository_mock = MagicMock()
 
-        artist_lyrics_service = ArtistLyricsService(lyrics_searcher_mock, statistic_mock, repository_mock)
+        artist_lyrics_service = ArtistLyricsService(lyrics_searcher_mock,
+                                                    statistic_mock,
+                                                    repository_mock,
+                                                    artist_searcher_mock)
         artist_lyrics_service.index('Weiss')
 
         call1 = mock.call(lyrics_mock1)
