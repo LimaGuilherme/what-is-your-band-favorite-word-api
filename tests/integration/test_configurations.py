75fa7dedfe2521a19c9d9e8e34f5421329d308ec
Leonardo Coelho <leonardo.coelho@inceres.com.br>
2020-12-04 15:53:51 -0300

    Implement tests to EnvFullConfigRepository

--- tests/integration/test_configurations.py
+++ tests/integration/test_configurations.py
@@ -1,7 +1,52 @@
 from unittest import TestCase, mock
 
-from src.configurations import LocalStorageSimpleConfigRepository, SimpleConfig
+from src.configurations import LocalStorageSimpleConfigRepository, EnvFullConfigRepository, SimpleConfig
 from src.exceptions import ConfigError
+
+
+class TestEnvFullConfigRepository(TestCase):
+
+    @mock.patch('src.configurations.os')
+    def test_should_get(self, os_mock):
+        os_mock.environ = {
+            'SPOTIFY_CLIENT_ID': 'V1',
+            'SPOTIFY_CLIENT_SECRET': 'V2',
+            'ELASTICSEARCH_HOST': 'V3',
+            'ELASTICSEARCH_PORT': 'V4',
+            'GENIUS_ACCESS_TOKEN': 'V5',
+            'MONGO_HOST': 'V6',
+            'MONGO_PORT': 'V7',
+            'REPOSITORY': 'V8',
+            'ELASTICSEARCH_INDEX': 'V9',
+            'MONGO_COLLECTION': 'V10'
+        }
+
+        repository = EnvFullConfigRepository()
+        full_config = repository.get()
+
+        self.assertFalse(full_config.DEBUG)
+        self.assertEqual(full_config.SPOTIFY_CLIENT_ID, 'V1')
+        self.assertEqual(full_config.SPOTIFY_CLIENT_SECRET, 'V2')
+        self.assertEqual(full_config.ELASTICSEARCH_HOST, 'V3')
+        self.assertEqual(full_config.ELASTICSEARCH_PORT, 'V4')
+        self.assertEqual(full_config.GENIUS_ACCESS_TOKEN, 'V5')
+        self.assertEqual(full_config.MONGO_HOST, 'V6')
+        self.assertEqual(full_config.MONGO_PORT, 'V7')
+        self.assertEqual(full_config.REPOSITORY, 'V8')
+        self.assertEqual(full_config.ELASTICSEARCH_INDEX, 'V9')
+        self.assertEqual(full_config.MONGO_COLLECTION, 'V10')
+
+    @mock.patch('src.configurations.os')
+    def test_get_should_raise_config_error_when_any_attribute_env_is_missing(self, os_mock):
+        os_mock.environ = {
+            'SPOTIFY_CLIENT_ID': 'V1',
+            'SPOTIFY_CLIENT_SECRET': 'V2',
+            'ELASTICSEARCH_HOST': 'V3'
+        }
+
+        with self.assertRaises(ConfigError):
+            repository = EnvFullConfigRepository()
+            repository.get()
 
 
 class TestLocalFileSimpleConfigRepository(TestCase):
