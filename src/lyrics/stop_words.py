7cf132c5efda1e02a37703cde963b9c944a24d9a
guilherme <guilherme@inceres.com.br>
2020-12-09 15:16:44 -0300

    Add pt-br stop words

--- src/lyrics/stop_words.py
+++ src/lyrics/stop_words.py
@@ -140,8 +140,8 @@ STOP_WORDS = {
     "sou", "sua", "suas", "talvez", "tanta", "tantas", "tanto", "tão", "tarde", "te", "temos", "tendes", "tens", "ter",
     "terceira", "terceiro", "teus", "tivemos", "tiveram", "tiveste", "tivestes", "toda", "todas", "todo", "três", "treze",
     "tua", "tuas", "tudo", "vai", "vais", "vão", "vários", "vem", "vêm", "vens", "vez", "vezes", "viagem", "vindo",
-    "vinte", "vocês", "vos", "vós", "vossa", "vossas", "vosso", "vossos", "zero", "[chorus]", "[verse]", "[verse", "-", "[chorus",
+    "vinte", "vocês", "vos", "vós", "vossa", "vossas", "vosso", "vossos", "zero", "[chorus]", "[verse]", "[verse", "-",
     "1]", "[pre-chorus]", "3]", "oh,", "pra", "tá", "vou", "fiz", "chorus", "hey", "2", "1", "", "1", "2",
     "3", "4", "5", "6", "7", "8", "9", "10", "11", "instrumental", "ive", "ill", "dont", "gonna", "verse",
-    "yeah", "aint", "eh", "ohoh", "ooh", "intro", "cmon"
+    "yeah", "aint", "eh", "ohoh", "ooh", "intro", "cmon", "falo", "pro", "prá", "manda", "mandar", "[chorus",
 }
