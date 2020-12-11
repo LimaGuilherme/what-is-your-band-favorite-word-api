b9ffce16b01cd94b940cbcb2180e6a83b14590a4
guilherme <guilherme@inceres.com.br>
2020-12-11 14:50:57 -0300

    Make Cli Test to config credentials.

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -1,11 +1,52 @@
+import json
+import os
 from unittest import TestCase
-from src.lyrics.cli_commands import config_credentials, get_top_words
-from src.lyrics.searchers import AlbumsSearcher
 
-from src import configurations as config_module
+from click.testing import CliRunner
+
+from src.lyrics.cli_commands import config_credentials, get_top_words
 
 
 class TestCLI(TestCase):
 
-    def test_should_get_top_words(self):
-        pass
+    def setUp(self) -> None:
+        self.cli_runner = CliRunner()
+
+    def tearDown(self, ) -> None:
+        try:
+            os.remove('.localstorage')
+        except OSError:
+            pass
+
+    def assert_file_exists(self):
+        self.assertTrue(os.path.exists('.localstorage'))
+
+    def assert_credentials_in_localstorage(self, spotify_client_id, spotify_client_secret, genius_access_token):
+        with open('.localstorage', 'r') as localstorage:
+            localstorage_credentials = json.load(localstorage)
+
+        self.assertIn('SPOTIFY_CLIENT_ID', localstorage_credentials)
+        self.assertIn('SPOTIFY_CLIENT_SECRET', localstorage_credentials)
+        self.assertIn('GENIUS_ACCESS_TOKEN', localstorage_credentials)
+
+        self.assertEqual(localstorage_credentials['SPOTIFY_CLIENT_ID'], spotify_client_id)
+        self.assertEqual(localstorage_credentials['SPOTIFY_CLIENT_SECRET'], spotify_client_secret)
+        self.assertEqual(localstorage_credentials['GENIUS_ACCESS_TOKEN'], genius_access_token)
+
+    def test_should_create_credentials_using_prompt(self):
+        self.cli_runner.invoke(config_credentials, input='a\nb\nc')
+        self.assert_file_exists()
+        self.assert_credentials_in_localstorage(spotify_client_id='a', spotify_client_secret='b', genius_access_token='c')
+
+    def test_should_create_credentials_using_options(self):
+        self.cli_runner.invoke(config_credentials, ['--spotify-client-id', 'a',
+                                                    '--spotify-client-secret', 'b',
+                                                    '--genius-access-token', 'c'])
+        self.assert_file_exists()
+        self.assert_credentials_in_localstorage(spotify_client_id='a', spotify_client_secret='b', genius_access_token='c')
+
+    def test_should_exit_when_given_a_invalid_key_using_options(self):
+        result = self.cli_runner.invoke(config_credentials, ['--spotify-client-id', 'a',
+                                                             '--spotify-client-secret', 'b',
+                                                             '--whatever-key', 'c'])
+        self.assertTrue(result.exit_code, 2)
