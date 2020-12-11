8e41ef9d33cbfce7408ef69b1897647ae3bcd5cc
guilherme <guilherme@inceres.com.br>
2020-12-11 16:50:18 -0300

    Remove unused exceptions.py

--- src/lyrics/exceptions.py
+++ src/lyrics/exceptions.py
@@ -1,31 +1,3 @@
-class BadParameter(Exception):
-    pass
-
-
-class NotAuthorized(Exception):
-    pass
-
-
-class UnexpectedError(Exception):
-    pass
-
-
-class NotFound(Exception):
-    pass
-
-
-class RepositoryError(Exception):
-    pass
-
-
-class ConfigClassNotFound(Exception):
-    pass
-
-
-class AlbumsNotFound(Exception):
-    pass
-
-
 class LyricsNotFound(Exception):
     pass
 
